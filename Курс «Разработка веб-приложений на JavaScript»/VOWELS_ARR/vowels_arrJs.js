35bb943c85ca5852b64f547cb8dae8e5c9208fb9
galakhevgeniy <nayk333@gmail.com>
2019-05-30 00:52:47 +0300

    Залил VOWELS_ARR

---
+++ Курс «Разработка веб-приложений на JavaScript»/VOWELS_ARR/vowels_arrJs.js
@@ -0,0 +1,43 @@
+"use strict";
+
+(function() {
+do{
+    
+    var str = prompt("Введите строку");
+    
+} while(!str);
+
+var listOfVawelsRus = {а:true, о:true, у:true, ы:true, э:true, я:true, е:true, ё:true, ю:true, и:true};
+
+function SearchVowelsRUSForEach(str,listOfVawelsRus)
+{
+    var count =0;
+    var strArray = str.split('');
+    function d (v,i,a)
+    {
+        if (v in listOfVawelsRus)
+        { 
+        return count++; 
+        }
+    }
+    strArray.forEach(d);
+    return count;
+}
+
+function SearchVowelsRUSfilter(str,listOfVawelsRus)
+{
+    var strArray=str.split('');
+    return strArray.filter((v,i,a)=>v in listOfVawelsRus).length;
+}
+
+var countUsedReduse = str.split('').reduce(function(count, elem) 
+    {
+        if(elem in listOfVawelsRus)return ++count;
+        else return count;
+    },0);
+
+
+alert("Количество гласных букв в вашей строке (forEach) = "+SearchVowelsRUSForEach(str,listOfVawelsRus));
+alert("Количество гласных букв в вашей строке (filter) = "+SearchVowelsRUSfilter(str,listOfVawelsRus));
+alert("Количество гласных букв в вашей строке (reduse) = "+countUsedReduse);
+})( );  
